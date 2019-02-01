f5c7dd4b28f2b476138c9c597337fa7d6fc5845f
nathaniel <348831271@qq.com>
2019-02-02 04:12:05 +0800

    ,

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const {ungzip} = require('node-gzip');
 
+const fs = require('fs');
+
+async function readPath(path='./data'){
+    filenames=fs.readDirSync(path);
+    console.log(filenames)
+    return ;
+}
+
+
+
+
+readPath();
