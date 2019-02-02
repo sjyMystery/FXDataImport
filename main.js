0630851d831c77fa0ec49524406fe18462400da7
nathaniel <348831271@qq.com>
2019-02-02 16:30:55 +0800

    ,

--- main.js
+++ main.js
@@ -41,13 +41,14 @@ async function handleFile(root,type,year,filename){
          * Here , we've ungzipped this data, and trying to format it.
          */
             const parsed_data = await parse_data(solved_data.toString(), type);
-            HistoryPrice.bulkCreate(parsed_data).then(
-                result=>{
-                    console.log(`saving compelete:${type},${year},${filename} total:${j++}/${i}`)
-                }
-            ).catch(error=>{
-                console.log(`saving error:${type},${year},${filename} ${error.message}`)
-            })
+            console.log(parsed_data)
+            // HistoryPrice.bulkCreate(parsed_data).then(
+            //     result=>{
+            //         console.log(`saving compelete:${type},${year},${filename} total:${j++}/${i}`)
+            //     }
+            // ).catch(error=>{
+            //     console.log(`saving error:${type},${year},${filename} ${error.message}`)
+            // })
             console.log(`parse compelete:${type},${year},${filename} total:${i++}`)
         }
         catch (e) {
